6cbe7789367cc537f24c2ae543d08a5f29743cc8
AnthonyNg404 <66294553+AnthonyNg404@users.noreply.github.com>
2020-06-19 21:55:31 -0700

    Add files via upload

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class IntListTest {
@@ -64,6 +63,17 @@ public class IntListTest {
         IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
         assertEquals(exp, IntList.catenate(A, B));
         assertEquals(IntList.of(1, 2, 3), A);
+    }
+
+    @Test(timeout = 5000)
+    public void reverse() {
+        IntList empty = IntList.of();
+        assertEquals(null, IntList.reverse(empty));
+        IntList A = IntList.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        IntList B = IntList.of(9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+        IntList C = IntList.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(B, IntList.reverse(A));
+        assertNotEquals(C, A);
     }
 
     /** If you're running this from the command line, you'll need
