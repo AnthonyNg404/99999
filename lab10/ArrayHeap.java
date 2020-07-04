eb37d4462b6bf8d77ea51eb06704ec58cbf885e7
AnthonyNg404 <66294553+AnthonyNg404@users.noreply.github.com>
2020-07-04 00:28:56 -0700

    Add files via upload

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -102,10 +102,10 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private void swim(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
-        validateSinkSwimArg(index);
         if (index == 1) {
             return;
         }
+        validateSinkSwimArg(index);
         if (contents[index].myPriority < contents[parentIndex(index)].myPriority) {
             swap(index, parentIndex(index));
             swim(parentIndex(index));
@@ -117,10 +117,10 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private void sink(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
-        validateSinkSwimArg(index);
         if (index > size / 2) {
             return;
         }
+        validateSinkSwimArg(index);
         int min = min(leftIndex(index), rightIndex(index));
         if (contents[index].myPriority > contents[min].myPriority) {
             swap(index, min);
@@ -194,6 +194,11 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     }
 
     private void changeHelper(int index, T item, double priority) {
+        if (index < 1 || index > size) {
+            return;
+        } else if (contents[index] == null) {
+            return;
+        }
         while (index <= size) {
             if (contents[index].myItem.equals(item)) {
                 contents[index].myPriority = priority;
