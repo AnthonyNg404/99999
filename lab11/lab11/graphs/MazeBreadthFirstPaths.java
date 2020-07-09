dfd753e7358d4d9bc8919f617186d58e4315c1d7
AnthonyNg404 <66294553+AnthonyNg404@users.noreply.github.com>
2020-07-08 19:49:38 -0700

    Add files via upload

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,29 +1,51 @@
 package lab11.graphs;
-
+import java.util.Queue;
+import java.util.LinkedList;
 /**
  *  @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
-    /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
-    */
+    // Inherits public fields:
+    private Queue<Integer> fringe;
+    private int start;
+    private int end;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        start = m.xyTo1D(sourceX, sourceY);
+        end = m.xyTo1D(targetX, targetY);
+        fringe = new LinkedList<>();
+        fringe.add(start);
+        marked[start] = true;
+        distTo[start] = 0;
+        edgeTo[start] = start;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        while (!fringe.isEmpty()) {
+            int v = fringe.poll();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    distTo[w] = distTo[v] + 1;
+                    edgeTo[w] = v;
+                    announce();
+                    if (w == end) {
+                        return;
+                    } else {
+                        fringe.add(w);
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,4 +1,5 @@
 package lab11.graphs;
+import java.util.Random;
 
 /**
  *  @author Josh Hug
@@ -9,6 +10,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] start;
+    private boolean foundCircle = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,7 +19,39 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        start = new int[maze.V()];
+
+        /* Set point where circle search starts */
+        Random rand = new Random();
+        int startX = rand.nextInt(maze.N());
+        int startY = rand.nextInt(maze.N());
+        int s = maze.xyTo1D(startX, startY);
+        marked[s] = true;
+        start[s] = s;
+        dfs(s);
+        announce();
+    }
+
+    private void dfs(int v) {
+        for (int w : maze.adj(v)) {
+            if (foundCircle) {
+                return;
+            }
+            if (!marked[w]) {
+                marked[w] = true;
+                start[w] = v;
+                dfs(w);
+            } else if (w != start[v]) {
+                int cur = v;
+                edgeTo[cur] = start[cur];
+                while (cur != w) {
+                    cur = start[cur];
+                    edgeTo[cur] = start[cur];
+                }
+                foundCircle = true;
+                return;
+            }
+        }
     }
 
     // Helper methods go here
