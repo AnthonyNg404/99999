b7e57f79aec6b61dbbf754f8a0301f532fea37c6
AnthonyNg404 <66294553+AnthonyNg404@users.noreply.github.com>
2020-07-17 00:17:30 -0700

    Add files via upload

--- lab13/CountingSort.java
+++ lab13/CountingSort.java
@@ -1,4 +1,4 @@
-/**
+/**Class with 2 ways of doing Counting sort, one naive way and one "better" way.
  * Class with 2 ways of doing Counting sort, one naive way and one "better" way
  *
  * @author Akhil Batra, Alexander Hwang
@@ -6,7 +6,8 @@
  **/
 public class CountingSort {
     /**
-     * Counting sort on the given int array. Returns a sorted version of the array.
+     * Counting sort on the given int array.
+     * Returns a sorted version of the array.
      * Does not touch original array (non-destructive method).
      * DISCLAIMER: this method does not always work, find a case where it fails
      *
@@ -14,20 +15,16 @@ public class CountingSort {
      * @return the sorted array
      */
     public static int[] naiveCountingSort(int[] arr) {
-        // find max
         int max = Integer.MIN_VALUE;
         for (int i : arr) {
             max = max > i ? max : i;
         }
 
-        // gather all the counts for each value
         int[] counts = new int[max + 1];
         for (int i : arr) {
             counts[i]++;
         }
 
-        // when we're dealing with ints, we can just put each value
-        // count number of times into the new array
         int[] sorted = new int[arr.length];
         int k = 0;
         for (int i = 0; i < counts.length; i += 1) {
@@ -36,8 +33,6 @@ public class CountingSort {
             }
         }
 
-        // however, below is a more proper, generalized implementation of
-        // counting sort that uses start position calculation
         int[] starts = new int[max + 1];
         int pos = 0;
         for (int i = 0; i < starts.length; i += 1) {
@@ -52,21 +47,47 @@ public class CountingSort {
             sorted2[place] = item;
             starts[item] += 1;
         }
-
-        // return the sorted array
         return sorted;
     }
 
     /**
-     * Counting sort on the given int array, must work even with negative numbers.
+     * Counting sort on the given int array,
+     * must work even with negative numbers.
      * Note, this code does not need to work for ranges of numbers greater
      * than 2 billion.
      * Does not touch original array (non-destructive method).
      *
      * @param arr int array that will be sorted
+     * @return xxxxxx
      */
     public static int[] betterCountingSort(int[] arr) {
-        // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        int[] counts = new int[max - min + 1];
+        for (int i : arr) {
+            counts[i - min]++;
+        }
+
+        int[] sorted = new int[arr.length];
+        int k = 0;
+        for (int i = 0; i < counts.length; i += 1) {
+            for (int j = 0; j < counts[i]; j += 1, k += 1) {
+                sorted[k] = i + min;
+            }
+        }
+        return sorted;
     }
 }
--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -1,23 +1,42 @@
-/**
+
+/**.
  * Class for doing Radix sort
  *
  * @author Akhil Batra, Alexander Hwang
  *
  */
 public class RadixSort {
-    /**
-     * Does LSD radix sort on the passed in array with the following restrictions:
+    /**.
+     */
+
+    private static final int RADIX = 256;
+    /**.
+     * Does LSD radix sort on the passed
+     * in array with the following restrictions:
      * The array can only have ASCII Strings (sequence of 1 byte characters)
      * The sorting is stable and non-destructive
-     * The Strings can be variable length (all Strings are not constrained to 1 length)
+     * The Strings can be variable length
+     * (all Strings are not constrained to 1 length)
      *
      * @param asciis String[] that needs to be sorted
      *
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        return null;
+        if (asciis.length == 0) {
+            return asciis;
+        }
+
+        int maxLength = 0;
+        for (String s: asciis) {
+            maxLength = Math.max(maxLength, s.length());
+        }
+
+        String[] sorted = asciis.clone();
+        for (int i = maxLength - 1; i >= 0; i--) {
+            sortHelperLSD(sorted, i);
+        }
+        return sorted;
     }
 
     /**
@@ -27,22 +46,52 @@ public class RadixSort {
      * @param index The position to sort the Strings on.
      */
     private static void sortHelperLSD(String[] asciis, int index) {
-        // Optional LSD helper method for required LSD radix sort
-        return;
+        int[] count = new int[RADIX];
+        for (String s: asciis) {
+            if (s.length() < index + 1) {
+                count[0] += 1;
+            } else {
+                int pos = s.charAt(index);
+                count[pos] += 1;
+            }
+        }
+
+        int[] start = new int[RADIX];
+        int pos = 0;
+        for (int i = 0; i < start.length; i++) {
+            start[i] = pos;
+            pos += count[i];
+        }
+
+        String[] arrayBackup = asciis.clone();
+        for (String s: arrayBackup) {
+            int item = 0;
+            if (s.length() >= index + 1) {
+                item = s.charAt(index);
+            }
+            int place = start[item];
+            asciis[place] = s;
+            start[item] += 1;
+        }
     }
 
     /**
-     * MSD radix sort helper function that recursively calls itself to achieve the sorted array.
-     * Destructive method that changes the passed in array, asciis.
+     * MSD radix sort helper function that recursively
+     * calls itself to achieve the sorted array.
+     * Destructive method that changes the passed
+     * in array, asciis.
      *
      * @param asciis String[] to be sorted
-     * @param start int for where to start sorting in this method (includes String at start)
-     * @param end int for where to end sorting in this method (does not include String at end)
-     * @param index the index of the character the method is currently sorting on
+     * @param start int for where to start sorting
+     *              in this method (includes String at start)
+     * @param end int for where to end sorting
+     *            in this method (does not include String at end)
+     * @param index the index of the character
+     *              the method is currently sorting on
      *
      **/
-    private static void sortHelperMSD(String[] asciis, int start, int end, int index) {
-        // Optional MSD helper method for optional MSD radix sort
+    private static void
+        sortHelperMSD(String[] asciis, int start, int end, int index) {
         return;
     }
 }
